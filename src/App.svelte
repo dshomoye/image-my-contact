28926ee3a08c6125740e3943a9e3eef109372f07
Damola <dshomoye@gmail.com>
2020-10-21 14:21:51 -0700

    contact parsing done!

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,70 @@
 <script>
-	import "./Tailwindcss.svelte"
-	import ContactCard from "./ContactCard.svelte"
+  import "./Tailwindcss.svelte";
+  import ContactCard from "./ContactCard.svelte";
+  import vCard from "vcf";
+  import cntl from "cntl";
+  import UploadIcon from "./UploadIcon.svelte";
+
+  const fReader = new FileReader();
+  /**@type {vCard[]} */
+  let contacts = [];
+
+  const uploadBtnStyle = cntl`
+	flex flex-col items-center p-4 bg-white 
+	text-blue rounded-lg shadow-sm tracking-wide 
+	uppercase border border-blue cursor-pointer 
+	hover:bg-blue-400 hover:text-white hover:shadow-lg
+	transform hover:scale-105 transition duration-200
+	`;
+
+  fReader.onload = (e) => {
+    if (e.target.readyState != 2) return;
+    if (e.target.error) {
+      alert("Error Reading file.");
+      return;
+    }
+    try {
+      contacts = vCard.parse(e.target.result);
+    } catch (error) {
+      alert("Error reading contact(s).");
+    }
+  };
 </script>
 
-
 <main class="container text-center mx-auto px-24">
-  <h1 class="uppercase text-red-500 font-bold text-5xl my-12 underline">🖼 Image 💪🏾 Contact📇</h1>
-	<div class="grid grid-cols-2 lg:grid-cols-3 gap-12">
-		<ContactCard name="Jane Doe"/>
-		<ContactCard name="Rost"/>
-	</div>
+  <h1 class="uppercase text-red-500 font-bold text-5xl mt-12 mb-4 underline">
+    🖼 Image 💪🏾 Contact 📇
+  </h1>
+  {#if contacts && contacts.length > 0}
+    <div>
+      <p class="text-sm">Start Over?</p>
+      <button
+        on:click={() => (contacts = [])}
+        class="px-3 py-2 rounded shadow-sm m-1 mb-5 transition duration-200 hover:bg-red-400 transform hover:scale-105">Clear
+        ❌
+      </button>
+    </div>
+  {:else}
+    <div>
+      <p class="text-md py-4 lowercase">Click Below to continue</p>
+      <div class="flex items-center justify-center bg-grey-lighter mb-12">
+        <label class={uploadBtnStyle}>
+          <UploadIcon />
+          <span class="mt-2 text-sm leading-normal">Select vcard file</span>
+          <input
+            type="file"
+            class="hidden"
+            on:change={(e) => fReader.readAsText(e.target.files[0])}
+            accept=".vcf" />
+        </label>
+      </div>
+    </div>
+  {/if}
+  <div class="grid grid-cols-2 lg:grid-cols-3 gap-12">
+    {#each contacts as contact, i}
+      {#if contact.data.fn.valueOf() && true}
+        <ContactCard name={contact.data.fn.valueOf()} />
+      {/if}
+    {/each}
+  </div>
 </main>
--- src/ContactCard.svelte
+++ src/ContactCard.svelte
@@ -1,42 +1,51 @@
 <script>
   import "a-avataaar";
+  import { blur } from "svelte/transition";
   import cntl from "cntl";
   import { onMount } from "svelte";
   import { updateSVgWithStyle } from "./utils";
   import Canvg from "canvg";
+  import DownloadIcon from "./DownloadIcon.svelte";
 
-  export let name = "John Smith";
+  export const name = "John Smith";
+  export const contactIndex = null;
   let svgFile = "";
   let canvasRef;
   let svgString;
   let svgCtx;
   let pngBlob;
-  let applyImage = false
+  let applyImage = false;
 
   onMount(() => {
     svgCtx = canvasRef.getContext("2d");
   });
+  let cardStyle = "";
+  let btnStyle = "";
 
-  const cardStyle = cntl`
-  p-12 pb-5 border border-gray-300 rounded
-  shadow-sm -mt-8 hover hover:shadow-md 
-  transition duration-200 ease-in card
+  $: {
+    cardStyle = cntl`
+      p-12 pb-5 border border-gray-300 rounded
+      shadow-sm -mt-8 hover hover:shadow-md 
+      transition duration-200 ease-in card
+      ${applyImage ? `bg-green-200` : `bg-white`}
   `;
-  const btnStyle = cntl`
-  shadow bg-transparent hover:bg-blue-400
-  hover:text-white focus:shadow-outline 
-  focus:outline-none px-4 py-2 rounded 
-  text-sm cursor-pointer no-underline 
-  hover:no-underline
-  `;
-
-  const setImageBlob = () => {
-    if (applyImage) {
-      Canvg.fromString(svgCtx, svgString).render()
-      pngBlob = canvasRef.toDataURL("image/png")
-      console.log('set image blob')
-    }
+    btnStyle = cntl`
+      shadow bg-transparent hover:bg-blue-400
+      hover:text-white focus:shadow-outline 
+      focus:outline-none px-4 py-2 rounded 
+      text-sm cursor-pointer no-underline 
+      hover:no-underline ${applyImage ? `bg-white` : `bg-transparent`}
+      `;
   }
+
+  const setImageBlob = (event) => {
+    applyImage = event.target.checked;
+    if (applyImage) {
+      Canvg.fromString(svgCtx, svgString).render();
+      pngBlob = canvasRef.toDataURL("image/png");
+      console.log("set image blob");
+    }
+  };
 
   const handleSvgChange = async (event) => {
     svgString = updateSVgWithStyle(event);
@@ -53,15 +62,17 @@
   }
 </style>
 
-<div class="transform hover:scale-105 transition duration-200 ease-in">
+<div
+  class="transform hover:scale-105 transition duration-200 ease-in"
+  transition:blur>
   <div class="rounded-full">
     <a-avataaar identifier={name} on:svgchange={handleSvgChange} />
   </div>
   <div class={cardStyle}>
     <p>{name}</p>
-    <hr />
-    <div class="flex space-x-5 mt-5 items-center">
-      <div>
+    <hr class="border-gray-500" />
+    <div class="flex mt-5 items-center">
+      <div class="m-auto">
         <a
           class={btnStyle}
           type="button"
@@ -69,21 +80,21 @@
           href={svgFile}
           target="_blank"
           download={`${name}-avatar.svg`}>
-          Save
+          <DownloadIcon />
         </a>
       </div>
-      <label
-        class="text-gray-600 text-sm hover:text-blue-800 items-center"
-        title="Apply image to contact in vcard.">
-        <input
-          class="mr-1 leading-tight"
-          type="checkbox"
-          download={`${name}-avatar.svg`}
-          bind:value={applyImage}
-          on:change={setImageBlob}
-        />
-        <span>Apply</span>
-      </label>
+      <div class="m-auto">
+        <label
+          class="text-gray-600 text-sm hover:text-blue-800 items-center m-auto"
+          title="Apply image to contact in vcard.">
+          <input
+            class="mr-1 leading-tight"
+            type="checkbox"
+            download={`${name}-avatar.svg`}
+            on:change={setImageBlob} />
+          <span>{applyImage ? 'Remove' : 'Update'}</span>
+        </label>
+      </div>
     </div>
   </div>
   <div class="hidden"><canvas bind:this={canvasRef} /></div>
